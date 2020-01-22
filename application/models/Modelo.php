84cfb56c5253d3c8bb4a99a3feb11c78e085d8ce
3DWIN13 <ecolladocastillo@gmail.com>
2020-01-22 00:37:11 -0400

    Create Modelo.php

---
+++ application/models/Modelo.php
@@ -0,0 +1,29 @@
+<?php
+class Modelo extends CI_Model {
+    public function __construct()
+	{
+		parent::__construct();	
+        $this->load->database();
+		
+    }
+
+    function crearTabla($tablaC){
+         $this->db->query("CREATE TABLE IF NOT EXISTS `?` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `location` varchar(100) NOT NULL,
+            `position` varchar(100) NOT NULL,
+            `company` varchar(100) NOT NULL,
+            `description` varchar(100) NOT NULL,
+            `howtoapply` varchar(200) NOT NULL,
+            `category` varchar(50) NOT NULL,
+            PRIMARY KEY (`id`)
+          )",$tablaC);
+    }
+
+     
+    }
+
+
+
+/* End of file Controllername.php */
+?>
