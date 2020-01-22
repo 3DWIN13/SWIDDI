59626d92e2a1063ea11f7798eb7322584b1e7c25
LisbethC11 <60081288+LisbethC11@users.noreply.github.com>
2020-01-22 22:16:50 +0100

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,13 +75,13 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'swiddi1',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'mysql',
 	'dbprefix' => '',
-	'pconnect' => FALSE,
+	'pconnect' => TRUE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
