1801712b6f0c88709f3587934d8d2e5bebb85783
LisbethC11 <60081288+LisbethC11@users.noreply.github.com>
2020-01-22 21:44:02 +0100

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] = array(
 	'database' => 'swiddi1',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => TRUE,
+	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
