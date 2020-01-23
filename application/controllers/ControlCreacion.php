c3c35157fcbbb086b093da511eb3423f1026929e
3DWIN13 <ecolladocastillo@gmail.com>
2020-01-22 23:54:41 -0400

    Update ControlCreacion.php

--- application/controllers/ControlCreacion.php
+++ application/controllers/ControlCreacion.php
@@ -15,10 +15,17 @@ class ControlCreacion extends CI_Controller {
 	{
 		$datos = array();
 
-		
-		$this->load->view('trabajosView.php');
+		$datos['swiddi1'] = $this->Modelo->cargar();
+		$this->load->view('trabajosView.php',$datos);
 	}
-	
+
+	public function guardar()
+	{
+		if($_POST){
+			$this->Modelo->guardar($_POST['location'],$_POST['position'],$_POST['company'],$_POST['description'],$_POST['howtoapply'],$_POST['cbo'],$_POST['cbo']);//location,position,company,description,howtoapply,category
+		}
+		$this->load->view('mensage');
+	}
 	
    
     
