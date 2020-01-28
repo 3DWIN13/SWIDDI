67d2d51ff8fd6b7f9ebfe44cb9915479886a23e5
3DWIN13 <ecolladocastillo@gmail.com>
2020-01-28 17:22:42 -0400

    Update login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -8,15 +8,7 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-        if(isset($_POST['password'])){
-        $this->load->model('usuario_model');
-        if($this->usuario_model->login($_POST['username'],md5($_POST['password']))){
-            redirect('InterfazA.php');
-        }else{
-            redirect('InterfazA.php');
-        }
-        }
-		$this->load->view('InterfazA.php');
+      
 	}
 }
 ?>
