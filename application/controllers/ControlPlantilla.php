4b8e0b9c0847e75516c7a94ea860798872b93d05
3DWIN13 <ecolladocastillo@gmail.com>
2020-01-28 20:51:08 -0400

    Update ControlPlantilla.php

--- application/controllers/ControlPlantilla.php
+++ application/controllers/ControlPlantilla.php
@@ -13,7 +13,19 @@ class ControlPlantilla extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('plantilla.php');
+              
+  //      echo $_POST['editar'];
+  $str = explode(",",$_POST['editar']);
+  //echo $str[1];
+  $str2 = "'$str[1]'";
+  //echo $str2;
+          $datos = array();
+  
+          $str3=$str[0];
+  
+          $datos['tabla'] = $this->Modelo->cargar3($str2,$str[0]);
+        
+		$this->load->view('plantilla.php',$datos);
 	}
 
 }
